f36c29fa3c55291a27b89b0ad9951aca35be3a0e
Egor <Egmell@yandex.ru>
2023-08-14 18:15:50 +0300

    stack

---
+++ correct_bracket_seq.go
@@ -0,0 +1,37 @@
+package main
+
+func correctBracketSeq(seq string) bool {
+	var stack []string
+	for i := 0; i < len(seq); i++ {
+		if seq[i] == '(' || seq[i] == '{' || seq[i] == '[' {
+			stack = append(stack, string(seq[i]))
+		} else {
+			brck := stack[len(stack)-1]
+
+			switch seq[i] {
+			case ')':
+				if brck == "(" {
+					stack = stack[:len(stack)-1]
+				} else {
+					return false
+				}
+			case ']':
+				if brck == "[" {
+					stack = stack[:len(stack)-1]
+				} else {
+					return false
+				}
+			case '}':
+				if brck == "{" {
+					stack = stack[:len(stack)-1]
+				} else {
+					return false
+				}
+			}
+		}
+	}
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
